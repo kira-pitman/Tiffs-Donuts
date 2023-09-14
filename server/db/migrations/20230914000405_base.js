58dd17cd725d84bee25d25250b91ab6c575419c1
Tiffany Wang <thiffanyjun@hotmail.com>
2023-09-15 10:24:40 +1200

    updating db table names

--- server/db/migrations/20230914000405_base.js
+++ server/db/migrations/20230914000405_base.js
@@ -1,11 +1,11 @@
 export function up(knex) {
-  return knex.schema.createTable("base", (table) => {
-    table.increments("id").primary();
-    table.string("color");
-    table.string("name");
-  });
+  return knex.schema.createTable('bases', (table) => {
+    table.increments('id').primary()
+    table.string('color')
+    table.string('name')
+  })
 }
 
 export function down(knex) {
-  return knex.schema.dropTableIfExists("base");
+  return knex.schema.dropTableIfExists('bases')
 }

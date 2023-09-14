a9039fa673128db3895032eea8d60933e9b3582a
Tiffany Wang <thiffanyjun@hotmail.com>
2023-09-14 15:10:16 +1200

    adding donutDetail component

---
+++ client/components/DonutDetails.jsx
@@ -0,0 +1,23 @@
+import DonutForm from './DonutForm'
+
+//need to display details i.e name, price? anything else?
+// all from the database
+
+function DonutDetails() {
+  const flavorArray = ['Chocolate', 'Strawberry', 'Green tea', 'Lemon']
+  const baseArray = ['Original', 'Chocolate']
+  const priceArray = ['8', '9', '7', '8']
+
+  //how to link to forms? import Donut Form function and use? idk
+
+  return (
+    <>
+    <h2>Donut Name</h2>
+
+    <p>___ base with ___ topping</p>
+    
+    
+    <h4>Price:</h4><p>price</p>
+    </>
+  )
+}

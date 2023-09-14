5e4a418dfa1d08fbe82f5ad5a3395c95b5e8e4cb
Nathalia Prado <nathalia_898@hotmail.com>
2023-09-14 12:17:28 +1200

    Add migration and seeds files

---
+++ server/db/seeds/1_base.js
@@ -0,0 +1,14 @@
+export async function seed(knex) {
+    return await knex('base').insert([
+        {
+            id: 1,
+            color: '0x7b3f00',
+            name: 'Chocolate',
+        },
+        {
+            id: 2,
+            color: '0xf57f8e',
+            name: 'Strawberry',
+        },
+    ])
+  }

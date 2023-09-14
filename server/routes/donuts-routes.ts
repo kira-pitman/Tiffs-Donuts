d12ade28ebeafabd3ed82703173fb8986fd04dc6
Tiffany Wang <thiffanyjun@hotmail.com>
2023-09-15 10:25:34 +1200

    updating back-end routes

--- server/routes/donuts-routes.ts
+++ server/routes/donuts-routes.ts
@@ -4,9 +4,9 @@ import * as db from '../db/donuts-db'
 const router = express.Router()
 
 // Get /api/v1/donuts/flavors
-router.get('/flavors', async (req, res) => {
+router.get('/glazes', async (req, res) => {
   try {
-    const flavorNames = await db.getAllFlavorNames()
+    const flavorNames = await db.getAllGlazes()
     res.json(flavorNames)
   } catch (error) {
     res.sendStatus(500)
@@ -17,30 +17,8 @@ router.get('/flavors', async (req, res) => {
 // Get /api/v1/donuts/bases
 router.get('/bases', async (req, res) => {
   try {
-    const baseNames = await db.getAllBaseNames()
+    const baseNames = await db.getAllBases()
     res.json(baseNames)
-  } catch (error) {
-    res.sendStatus(500)
-    console.error(error)
-  }
-})
-
-// Get /api/v1/donuts/bases/:name
-router.get('/bases/:name', async (req, res) => {
-  try {
-    const bases = await db.getBaseByName(req.params.name)
-    res.json(bases)
-  } catch (error) {
-    res.sendStatus(500)
-    console.error(error)
-  }
-})
-
-// Get /api/v1/donuts/flavors/:name
-router.get('/flavors/:name', async (req, res) => {
-  try {
-    const flavors = await db.getFlavorByName(req.params.name)
-    res.json(flavors)
   } catch (error) {
     res.sendStatus(500)
     console.error(error)

cdd42acd409292938226effd93da041cdd2a23e3
Tiffany Wang <thiffanyjun@hotmail.com>
2023-09-15 10:25:50 +1200

    updating ts models

--- models/donuts.ts
+++ models/donuts.ts
@@ -1,22 +1,12 @@
-export interface FlavorName {
-  id: number;
-  name: string;
-}
-
-export interface BaseName {
-  id: number;
-  name: string;
-}
-
-export interface Flavor {
-  id: number;
-  name: string;
-  color: string;
-  price: number;
+export interface Glaze {
+  id: number
+  name: string
+  color: string
+  price: number
 }
 
 export interface Base {
-  id: number;
-  name: string;
-  color: string;
+  id: number
+  name: string
+  color: string
 }

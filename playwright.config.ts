f072e3ec4e609b6991e732ccde560e65d122918b
popoxta <popoxta@gmail.com>
2023-09-15 15:25:00 +1200

    Update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,22 +30,24 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
+  expect: { timeout: 10000 },
+
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'browser',
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    //
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {

7313ee125d068f64448f7cbdf8c5dac2a834a34a
popoxta <popoxta@gmail.com>
2023-09-18 09:49:11 +1200

    Added more glazes/bases

--- server/db/seeds/1_bases.js
+++ server/db/seeds/1_bases.js
@@ -10,5 +10,60 @@ export async function seed(knex) {
       color: '#9e8a5d',
       name: 'Chocolate',
     },
+    {
+      id: 3,
+      color: '#fffdf8',
+      name: 'Milky'
+    },
+    {
+      id: 4,
+      color: '#eec9a0',
+      name: 'Cinnamon'
+    },
+    {
+      id: 5,
+      color: '#f9f385',
+      name: 'Lemon'
+    },
+    {
+      id: 6,
+      color: '#fc802d',
+      name: 'Pumpkin'
+    },
+    {
+      id: 7,
+      color: '#c93d2a',
+      name: 'Maple',
+    },
+    {
+      id: 8,
+      color: '#e38b96',
+      name: 'Strawberry',
+    },
+    {
+      id: 9,
+      color: '#89d396',
+      name: 'Matcha',
+    },
+    {
+      id: 10,
+      color: '#ad1a41',
+      name: 'Red Velvet',
+    },
+    {
+      id: 11,
+      color: '#d5cec8',
+      name: 'Oreo',
+    },
+    {
+      id: 12,
+      color: '#f0b46d',
+      name: 'Salted Caramel',
+    },
+    {
+      id: 13,
+      color: '#a2583d',
+      name: 'Pretzel'
+    }
   ])
 }
--- server/db/seeds/2_glazes.js
+++ server/db/seeds/2_glazes.js
@@ -2,7 +2,7 @@ export async function seed(knex) {
   return await knex('glazes').insert([
     {
       id: 1,
-      color: '#7b3f00',
+      color: '#7a431e',
       name: 'Chocolate',
       price: 8,
     },
@@ -24,5 +24,71 @@ export async function seed(knex) {
       name: 'Lemon',
       price: 8,
     },
+    {
+      id: 5,
+      color: '#fffbec',
+      name: 'Vanilla',
+      price: 5,
+    },
+    {
+      id: 6,
+      color: '#e5632d',
+      name: 'Maple',
+      price: 10,
+    },
+    {
+      id: 7,
+      color: '#e79c44',
+      name: 'Caramel',
+      price: 6.5,
+    },
+    {
+      id: 8,
+      color: '#e21864',
+      name: 'Raspberry',
+      price: 3,
+    },
+    {
+      id: 9,
+      color: '#567cec',
+      name: 'Blueberry',
+      price: 4,
+    },
+    {
+      id: 10,
+      color: '#a7d688',
+      name: 'Pistachio',
+      price: 15,
+    },
+    {
+      id: 11,
+      color: '#e7a97d',
+      name: 'Chai',
+      price: 11.5,
+    },
+    {
+      id: 12,
+      color: '#ffcf4c',
+      name: 'Honey',
+      price: 6,
+    },
+    {
+      id: 13,
+      color: '#ffcf4c',
+      name: 'Honey',
+      price: 6,
+    },
+    {
+      id: 12,
+      color: '#ffcf4c',
+      name: 'Honey',
+      price: 6,
+    },
+    {
+      id: 12,
+      color: '#ffcf4c',
+      name: 'Honey',
+      price: 6,
+    }
   ])
 }

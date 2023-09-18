6fb5e9d4f24e8c505478567ce0488511de95b7db
popoxta <popoxta@gmail.com>
2023-09-18 12:16:04 +1200

    Setup routes

--- client/main.jsx
+++ client/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/input.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Donut from "./pages/Donut.jsx";
+
+export const routes = createRoutesFromElements(
+    <Route path={'/'} element={<App/>}>
+        <Route index element={<Donut/>}/>
+    </Route>
+)
+
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 )

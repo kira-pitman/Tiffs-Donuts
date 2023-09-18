73a49ab4d513a56291e1a665d2062735f97ba339
popoxta <popoxta@gmail.com>
2023-09-19 10:34:16 +1200

    Add readme info

--- server/lib/errors.ts
+++ server/lib/errors.ts
@@ -2,6 +2,9 @@ const clientError = (res, message) => res.status(400).json({message})
 
 const notFoundError = (res, message) => res.status(404).json({message})
 
-const unauthorizedError = (res, message) => res.status(401).json({message})
+const unauthorizedError = (res, message) => {
+    console.log(res)
+    return res.status(401).json({message})
+}
 
 export default {clientError, notFoundError, unauthorizedError}

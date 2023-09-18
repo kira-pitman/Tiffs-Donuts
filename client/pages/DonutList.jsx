1cc12b0363161f4d85684da1cf2c4785c3e8d422
Nathalia Prado <nathalia_898@hotmail.com>
2023-09-19 11:42:17 +1200

    Change list empty message

--- client/pages/DonutList.jsx
+++ client/pages/DonutList.jsx
@@ -23,42 +23,12 @@ export default function DonutList() {
   if (isLoading) {
     return <p>Loading...</p>
   }
-
-  const donutsFakeList = [
-    {
-        id: 1,
-        base: 3,
-        baseName: 'Milky',
-        glaze: 1,
-        glazeName: 'Chocolate',
-        price: 8,
-        auth0_id: 'hello123',
-      },
-      {
-        id: 2,
-        base: 1,
-        baseName: 'Original',
-        glaze: 1,
-        glazeName: 'Chocolate',
-        price: 8,
-        auth0_id: 'hello123',
-      },
-      {
-        id: 3,
-        base: 2,
-        baseName: 'Chocolate',
-        glaze: 2,
-        glazeName: 'Strawberry',
-        price: 9,
-        auth0_id: 'world156',
-      }
-  ]
  
   return (
     <>
       <div className="flex flex-col items-center justify-center">
         <h2 className="text-5xl font-extrabold leading-snug mb-10">List of donuts</h2>
-        {donutsFakeList.length === 0 ? <h3 className="mb-2 text-2xl tracking-tight text-gray-900">List is empty</h3> : donutsFakeList.map(donut => <DonutCard key={donut.id} donut={donut}></DonutCard>)}
+        {data.length === 0 ? <h3 className="mb-2 text-2xl tracking-tight text-gray-900">Nothing here yet!</h3> : data.map(donut => <DonutCard key={donut.id} donut={donut}></DonutCard>)}
       </div>
     </>
   )

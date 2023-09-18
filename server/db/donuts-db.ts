15000d0c785b5467b7d359eb974e4410b6c0ae9a
popoxta <popoxta@gmail.com>
2023-09-18 16:08:07 +1200

    Add db methods

--- server/db/donuts-db.ts
+++ server/db/donuts-db.ts
@@ -3,20 +3,36 @@ import db from './connection'
 
 //Get all flavours
 export function getAllGlazes(): Promise<Glaze[]> {
-  const glazes = db('glazes').select()
-  return glazes
+    const glazes = db('glazes').select()
+    return glazes
 }
 
 //Get all base types
 export function getAllBases(): Promise<Base[]> {
-  const bases = db('bases').select()
-  return bases
+    const bases = db('bases').select()
+    return bases
 }
 
 export const getBase = (id) => {
-  return db('bases').select().where({id}).first()
+    return db('bases').select().where({id}).first()
 }
 
 export const getGlaze = (id) => {
     return db('glazes').select().where({id}).first()
+}
+
+export const getDonut = (id) => {
+    return db('donuts').select().where({id}).first()
+}
+
+export const getDonuts = (auth0Id) => {
+    return db('donuts').select().where({auth0_id: auth0Id})
+}
+
+export const insertDonut = (donut) => {
+    return db('donuts').insert(donut).returning('*')
+}
+
+export const deleteDonut = (id) => {
+    return db('donuts').delete().where({id})
 }

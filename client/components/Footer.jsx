5c4d5de44ac529d5b3225fff80fd930185d10867
Paul David Tan <cryxuz@gmail.com>
2023-09-20 10:12:17 +1200

    fix back to donut btn

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <p className="mx-auto w-full flex justify-center">The 3d model
         is created by Cresis from Sketchfab.com.
       </p>
-      <p className="mx-auto w-full flex justify-center">Nav icon is created by Triberion from <a href="https://www.flaticon.com/free-icons/donut" title="donut icons">Flaticon</a></p>
+      <p className="mx-auto w-full flex justify-center">Nav icon is created by Triberion from <a href="https://www.flaticon.com/free-icons/donut" title="donut icons"> Flaticon</a></p>
     </footer>
   );
 }

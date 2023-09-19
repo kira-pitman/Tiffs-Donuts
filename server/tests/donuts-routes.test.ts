dcce06cc4fdfbb23296ee433f530bf8f46836e84
popoxta <popoxta@gmail.com>
2023-09-19 15:18:53 +1200

    Formatting

--- server/tests/donuts-routes.test.ts
+++ server/tests/donuts-routes.test.ts
@@ -50,11 +50,13 @@ describe("GET /me", () => {
     await request(app).get("/api/v1/donuts/me").expect(401);
   });
 
-  it('Should return a 500 if an error occurs', async () => {
-    vi.spyOn(db, "getDonuts").mockRejectedValueOnce("NO DONUTS TODAY")
-    await request(app).get('/api/v1/donuts/me').expect(500)
-        .set("Authorization", `Bearer 123`)
-  })
+  it("Should return a 500 if an error occurs", async () => {
+    vi.spyOn(db, "getDonuts").mockRejectedValueOnce("NO DONUTS TODAY");
+    await request(app)
+      .get("/api/v1/donuts/me")
+      .expect(500)
+      .set("Authorization", `Bearer 123`);
+  });
 
   it("Should return user values if token is present", async () => {
     const res = await request(app)
@@ -100,106 +102,133 @@ describe("GET /me", () => {
   });
 });
 
-describe('GET /:id', () => {
-  it('Should return a donut', async () => {
-    const res = await request(app).get('/api/v1/donuts/4').expect(200)
-        .expect('Content-Type', /json/)
+describe("GET /:id", () => {
+  it("Should return a donut", async () => {
+    const res = await request(app)
+      .get("/api/v1/donuts/4")
+      .expect(200)
+      .expect("Content-Type", /json/);
 
     expect(res.body).toStrictEqual({
-          id: 4,
-          auth0_id: 'world156',
-          glazeId: 2,
-          baseId: 1,
-          glazeName: 'Strawberry',
-          price: 9,
-          glazeColor: '#f57f8e',
-          baseName: 'Original',
-          baseColor: '#e5e0cb'
-        }
-    )
-  })
+      id: 4,
+      auth0_id: "world156",
+      glazeId: 2,
+      baseId: 1,
+      glazeName: "Strawberry",
+      price: 9,
+      glazeColor: "#f57f8e",
+      baseName: "Original",
+      baseColor: "#e5e0cb",
+    });
+  });
 
-  it('Should return a 400 if the ID is invalid', async () => {
-    const res = await request(app).get('/api/v1/donuts/abcde').expect(400)
-    expect(res.body.message).toMatch(/invalid/i)
-  })
+  it("Should return a 400 if the ID is invalid", async () => {
+    const res = await request(app).get("/api/v1/donuts/abcde").expect(400);
+    expect(res.body.message).toMatch(/invalid/i);
+  });
 
-  it('Should return a 404 if the donut does not exist', async () => {
-    const res = await request(app).get('/api/v1/donuts/99').expect(404)
-    expect(res.body.message).toMatch(/does not exist/i)
-  })
+  it("Should return a 404 if the donut does not exist", async () => {
+    const res = await request(app).get("/api/v1/donuts/99").expect(404);
+    expect(res.body.message).toMatch(/does not exist/i);
+  });
 
-  it('Should return a 500 if an error occurs', async () => {
-    vi.spyOn(db, "getDonut").mockRejectedValueOnce("HE HAS BEEN EATEN")
-    await request(app).get('/api/v1/donuts/2').expect(500)
-  })
-})
+  it("Should return a 500 if an error occurs", async () => {
+    vi.spyOn(db, "getDonut").mockRejectedValueOnce("HE HAS BEEN EATEN");
+    await request(app).get("/api/v1/donuts/2").expect(500);
+  });
+});
 
-describe('DELETE /:id', () => {
-  it('Should delete a donut', async () => {
-    await request(app).delete("/api/v1/donuts/1")
-        .set("Authorization", `Bearer 123`).expect(200)
+describe("DELETE /:id", () => {
+  it("Should delete a donut", async () => {
+    await request(app)
+      .delete("/api/v1/donuts/1")
+      .set("Authorization", `Bearer 123`)
+      .expect(200);
 
-    const donut = await db.getDonut(1)
-    expect(donut).toBeUndefined()
-  })
+    const donut = await db.getDonut(1);
+    expect(donut).toBeUndefined();
+  });
 
-  it('Should return a 400 if the ID is invalid', async () => {
-    const res = await request(app).delete("/api/v1/donuts/abcde")
-        .set("Authorization", `Bearer 123`).expect(400)
-    expect(res.body.message).toMatch(/invalid/i)
-  })
+  it("Should return a 400 if the ID is invalid", async () => {
+    const res = await request(app)
+      .delete("/api/v1/donuts/abcde")
+      .set("Authorization", `Bearer 123`)
+      .expect(400);
+    expect(res.body.message).toMatch(/invalid/i);
+  });
 
-  it('Should return a 404 if the donut does not exist', async () => {
-    const res = await request(app).delete("/api/v1/donuts/99")
-        .set("Authorization", `Bearer 123`).expect(404)
-    expect(res.body.message).toMatch(/does not exist/i)
-  })
+  it("Should return a 404 if the donut does not exist", async () => {
+    const res = await request(app)
+      .delete("/api/v1/donuts/99")
+      .set("Authorization", `Bearer 123`)
+      .expect(404);
+    expect(res.body.message).toMatch(/does not exist/i);
+  });
 
-  it('Should return a 401 if the user is unauthorized', async () => {
-    const res = await request(app).delete("/api/v1/donuts/2").expect(401)
-    expect(res.body.message).toMatch(/unauthorized/i)
-  })
+  it("Should return a 401 if the user is unauthorized", async () => {
+    const res = await request(app).delete("/api/v1/donuts/2").expect(401);
+    expect(res.body.message).toMatch(/unauthorized/i);
+  });
 
-  it('Should return a 401 if the user id does not match', async () => {
-    const res = await request(app).delete("/api/v1/donuts/4")
-        .set("Authorization", `Bearer 123`).expect(401)
-    expect(res.body.message).toMatch(/unauthorized/i)
-  })
+  it("Should return a 401 if the user id does not match", async () => {
+    const res = await request(app)
+      .delete("/api/v1/donuts/4")
+      .set("Authorization", `Bearer 123`)
+      .expect(401);
+    expect(res.body.message).toMatch(/unauthorized/i);
+  });
 
-  it('Should return a 500 if an error occurs', async () => {
-    vi.spyOn(db, "deleteDonut").mockRejectedValueOnce("YOU CANT TAKE HIM")
-    await request(app).delete("/api/v1/donuts/2").set("Authorization", `Bearer 123`)
-        .expect(500)
-    const donutStillExists = await db.getDonut(2)
-    expect(donutStillExists).toBeDefined()
-  })
-})
+  it("Should return a 500 if an error occurs", async () => {
+    vi.spyOn(db, "deleteDonut").mockRejectedValueOnce("YOU CANT TAKE HIM");
+    await request(app)
+      .delete("/api/v1/donuts/2")
+      .set("Authorization", `Bearer 123`)
+      .expect(500);
+    const donutStillExists = await db.getDonut(2);
+    expect(donutStillExists).toBeDefined();
+  });
+});
 
-describe('POST /', () => {
-  const donut = {base: 1, glaze: 2}
+describe("POST /", () => {
+  const donut = { base: 1, glaze: 2 };
 
-  it('Should add a donut', async () => {
-    const res = await request(app).post("/api/v1/donuts/")
-        .set("Authorization", `Bearer 123`).send(donut).expect(200)
-    expect(res.body).toStrictEqual({ id: 6, base: 1, glaze: 2, auth0_id: 'hello123' })
-  })
+  it("Should add a donut", async () => {
+    const res = await request(app)
+      .post("/api/v1/donuts/")
+      .set("Authorization", `Bearer 123`)
+      .send(donut)
+      .expect(200);
+    expect(res.body).toStrictEqual({
+      id: 6,
+      base: 1,
+      glaze: 2,
+      auth0_id: "hello123",
+    });
+  });
 
-  it('Should return a 401 if the user is unauthorized', async () => {
-      const res = await request(app).post("/api/v1/donuts/")
-          .send(donut).expect(401)
-      expect(res.body.message).toMatch(/unauthorized/i)
-  })
+  it("Should return a 401 if the user is unauthorized", async () => {
+    const res = await request(app)
+      .post("/api/v1/donuts/")
+      .send(donut)
+      .expect(401);
+    expect(res.body.message).toMatch(/unauthorized/i);
+  });
 
-  it('Should return a 400 if not all details are added', async () => {
-      const res = await request(app).post("/api/v1/donuts/")
-          .set("Authorization", `Bearer 123`).send({glaze: 3}).expect(400)
-      expect(res.body.message).toMatch(/missing/i)
-  })
+  it("Should return a 400 if not all details are added", async () => {
+    const res = await request(app)
+      .post("/api/v1/donuts/")
+      .set("Authorization", `Bearer 123`)
+      .send({ glaze: 3 })
+      .expect(400);
+    expect(res.body.message).toMatch(/missing/i);
+  });
 
-  it('Should return a 500 if an error occurs', async () => {
-      vi.spyOn(db, "insertDonut").mockRejectedValueOnce("NO BAKING ALLOWED")
-      await request(app).post("/api/v1/donuts/").set("Authorization", `Bearer 123`)
-          .send(donut).expect(500)
-  })
-})
+  it("Should return a 500 if an error occurs", async () => {
+    vi.spyOn(db, "insertDonut").mockRejectedValueOnce("NO BAKING ALLOWED");
+    await request(app)
+      .post("/api/v1/donuts/")
+      .set("Authorization", `Bearer 123`)
+      .send(donut)
+      .expect(500);
+  });
+});
--- server/tests/test-setup.ts
+++ server/tests/test-setup.ts
@@ -1,9 +1,9 @@
-import {afterAll, beforeAll, beforeEach} from "vitest";
+import { afterAll, beforeAll, beforeEach } from "vitest";
 import db from "../db/connection";
 
 beforeAll(() => {
   return db.migrate.latest();
-})
+});
 
 // reseeds before each
 beforeEach(() => {

0dc1b2fcc800f9650fea248abb9f90661dc9d412
Jacob <jacobgranger56@gmail.com>
2023-09-19 15:27:08 +1200

    fixed some linting warnings

--- client/components/DonutCard.jsx
+++ client/components/DonutCard.jsx
@@ -1,13 +1,19 @@
 function DonutCard({ donut }) {
   return (
     <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mb-8">
-        <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/images/donut3.png" alt={`Donut base ${donut.glazeName}`} />
-        <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{donut.baseName} base with {donut.glazeName} topping</h5>
-            <p className="mb-3 font-normal text-gray-700">Price: ${donut.price}</p>
-        </div>
+      <img
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="/images/donut3.png"
+        alt={`Donut base ${donut.glazeName}`}
+      />
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+          {donut.baseName} base with {donut.glazeName} topping
+        </h5>
+        <p className="mb-3 font-normal text-gray-700">Price: ${donut.price}</p>
+      </div>
     </div>
-    )
-  }
-  
-  export default DonutCard
+  );
+}
+
+export default DonutCard;
--- client/components/SaveButton.jsx
+++ client/components/SaveButton.jsx
@@ -1,19 +1,16 @@
-import { useSearchParams } from 'react-router-dom'
-import { useAuth0 } from '@auth0/auth0-react'
-import { saveDonut } from '../api/apiClient'
+import { useAuth0 } from "@auth0/auth0-react";
+import { saveDonut } from "../api/apiClient";
 
 export default function SaveButton(props) {
-  // console.log(props.selectedBase.id, props.selectedGlaze.id)
-  // const [searchParams, setSearchParams] = useSearchParams()
-  const { getAccessTokenSilently } = useAuth0()
+  const { getAccessTokenSilently } = useAuth0();
 
   async function handleSave() {
     const donut = {
       glaze: props.selectedGlaze.id,
       base: props.selectedBase.id,
-    }
-    const token = await getAccessTokenSilently()
-    saveDonut(token, donut)
+    };
+    const token = await getAccessTokenSilently();
+    saveDonut(token, donut);
   }
   return (
     <button
@@ -22,5 +19,5 @@ export default function SaveButton(props) {
     >
       Save your donut
     </button>
-  )
+  );
 }

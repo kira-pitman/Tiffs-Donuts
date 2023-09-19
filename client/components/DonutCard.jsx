59154550ad51fe621b8200e842dc0e2801d7e5b4
Tiffany Wang <thiffanyjun@hotmail.com>
2023-09-20 10:03:21 +1200

    fix donut list responsiveness

--- client/components/DonutCard.jsx
+++ client/components/DonutCard.jsx
@@ -29,7 +29,7 @@ function DonutCard({ donut }) {
     <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mb-8">
       <div
         key={donut.id}
-        className="object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        className="object-cover w-full h-96 md:h-48 md:w-48 md:rounded-none md:rounded-l-lg"
       >
         <Canvas
           shadows
